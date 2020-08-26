8ab97acea4faae24f29cd50b6d95d4c6fb1cd431
Alexandr <wandering123456@gmail.com>
2020-08-27 02:26:38 +0300

    HelloWolrd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,56 @@
 package com.company;
-/*
- * Classname
- *
- * Version info
- * https://www.oracle.com/technetwork/java/codeconventions-150003.pdf
- * Copyright notice
- */
 
 public class Main {
 
-    public static void main(java.lang.String[] args) {
-        java.lang.String harryPotter = "Mr. and Mrs. Dursley, of number four, Privet Drive, were proud to say that they were perfectly normal, thank you very much. They were the last people you'd expect to be involved in anything strange or mysterious, because they just didn't hold with such nonsense. \n" +
-                "\n" +
-                "Mr. Dursley was the director of a firm called Grunnings, which made drills. He was a big, beefy man with hardly any neck, although he did have a very large mustache. Mrs. Dursley was thin and blonde and had nearly twice the usual amount of neck, which came in very useful as she spent so much of her time craning over garden fences, spying on the neighbors. The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere. \n" +
-                "\n" +
-                "The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was that somebody would discover it. They didn't think they could bear it if anyone found out about the Potters. Mrs. Potter was Mrs. Dursley's sister, but they hadn't met for several years; in fact, Mrs. Dursley pretended she didn't have a sister, because her sister and her good-for-nothing husband were as unDursleyish as it was possible to be. The Dursleys shuddered to think what the neighbors would say if the Potters arrived in the street. The Dursleys knew that the Potters had a small son, too, but they had never even seen him. This boy was another good reason for keeping the Potters away; they didn't want Dudley mixing with a child like that. \n" +
-                "\n" +
-                "When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story starts, there was nothing about the cloudy sky outside to suggest that strange and mysterious things would soon be happening all over the country. Mr. Dursley hummed as he picked out his most boring tie for work, and Mrs. Dursley gossiped away happily as she wrestled a screaming Dudley into his high chair. \n" +
-                "\n" +
-                "None of them noticed a large, tawny owl flutter past the window. ";
+    public static void main(String[] args) {
+        String palindrom = "Кара чар врача – рак";
+        String buffer = palindrom.toLowerCase(); //toLowerCase это делает все буквы на маленькие (нижний регистр)
+        System.out.println(buffer);
+        buffer = buffer.replaceAll(" ",""); //меняет regex: на replecement:
+        buffer = buffer.replaceAll("-",""); //меняет regex: на replecement:
+        buffer = buffer.replaceAll(",",""); //меняет regex: на replecement:
+        System.out.println(buffer);
+        String backward = "";
 
-        harryPotter = harryPotter.replaceAll(",", "");
-        harryPotter = harryPotter.replaceAll("\\.", "");// точку убирать только через левый двойно слеш
-
-        java.lang.String[] words = harryPotter.split(" ");
+        // GUI - grafic user interface - frontend
 
 
-        for (int i = 0; i < 47; i++) {
+        for (int i = buffer.length()-1; i >=0; i--) {
 
-            if (words[i].length()<=3) {
-                System.out.println( i+". "+words[i]); }
+            backward += buffer.charAt(i);
+
+
+
         }
+        System.out.println(backward);
+
+
+        boolean result = backward.equals(buffer);
+        System.out.println(result);
+
+        int[] arrey = new int[5];
+        arrey[0] = 10;
+        arrey[1] = 2;
+        arrey[2] = 3;
+        arrey[3] = 40;
+        arrey[4] = 50;
+
+        for (int i = 0; i < arrey.length; i++) {
+
+            if (arrey[i]>=10){
+                System.out.println(arrey[i]);
+            }
+
+
+        }
+        System.out.println(arrey[3]);
+
+        int ageOfMyFriend // это называется CamelCase !!!!
+
+        int areaOfMyGarden;
+
+
+
 
     }
 }
-//задание надом , найти знаки препинания и убрать .
